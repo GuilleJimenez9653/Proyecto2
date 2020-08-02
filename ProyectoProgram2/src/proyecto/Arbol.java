8bab8024207c64b8fd959ce035ed56d519c1e9ca
Guillermo Jimenez <jimenezguillermo1208@gmail.com>
2020-08-02 19:30:57 -0400

    Modifico la funcion de suma y agrego la funcion identificarPadre

--- ProyectoProgram2/src/proyecto/Arbol.java
+++ ProyectoProgram2/src/proyecto/Arbol.java
@@ -8,6 +8,11 @@ public class Arbol {
 		raiz = null;
 	}
 	
+	/**
+	 * Funcion para insertar un nodo en el arbol
+	 * @param indice se usa indice para saber el valor del indice del nodo que se desea ingresar, este es unico y no se debe repetir
+	 * @param valor valor contiene el numero que va a poseer el nodo
+	 */
 	public void insertar(int indice, int valor) {
 		Nodo nodo = new Nodo(indice);
 		nodo.setValor(valor);
@@ -31,6 +36,10 @@ public class Arbol {
 		}
 	}
 	
+	/**
+	 * Imprime el arbol con los nodos de forma ordenada 
+	 * @param nodo se le pasa el nodo raiz para recorrer el arbol
+	 */
 	public void recorrer(Nodo nodo) {
 		if(nodo != null) {
 			recorrer(nodo.getIzquierda());
@@ -39,6 +48,11 @@ public class Arbol {
 		}
 	}
 	
+	/**
+	 * Elimina un nodo y reorganiza los nodos
+	 * @param raiz se le pasa el nodo raiz para poder recorrer el arbol
+	 * @param nodo es el nodo que se desea eliminar
+	 */
 	public void eliminar(Nodo raiz ,Nodo nodo) {
         if(raiz==null) return;
 
@@ -54,6 +68,12 @@ public class Arbol {
             eliminar(raiz.getDerecha(),nodo);
         }
 	
+	/**
+	 * Busca si dos nodos tienen relacion padre hijo o hermanos
+	 * @param primer_nodo es el primer nodo que se selecciona en la interfaz grafica
+	 * @param segundo_nodo el segundo nodo que se selecciona e la interfaz grafica
+	 * @return retorna true si tiene alguna relacion, retorna false si no tienen ninguna relacion
+	 */
 	private boolean padreHijo(Nodo primer_nodo, Nodo segundo_nodo) {
 		boolean log = false;
 		if (primer_nodo.getDerecha()==segundo_nodo) {
@@ -70,10 +90,44 @@ public class Arbol {
 		return log;
 	}
 	
+	/**
+	 * devuelve el nodo padre de los dos nodos seleccionados
+	 * @param primer_nodo primer nodo seleccionado
+	 * @param segundo_nodo segundo nodo seleccionado
+	 * @return devuelve el nodo padre
+	 */
+	private Nodo identificarPadre(Nodo primer_nodo, Nodo segundo_nodo) {
+		Nodo nodo_padre = null;
+		if (primer_nodo.getPadre() == segundo_nodo) {
+			nodo_padre = segundo_nodo;
+		}else if(segundo_nodo.getPadre() == primer_nodo) {
+			nodo_padre = primer_nodo;
+		}else if(primer_nodo.getPadre() == segundo_nodo.getPadre()) {
+			nodo_padre = primer_nodo.getPadre();
+		}
+		return nodo_padre;
+	}
+	
+	/**
+	 * suma los valores de dos nodos y borra el primero que se le pasa
+	 * esta funcion debe revisarse 
+	 * @param primer_nodo es el primer nodo que se selecciona en la interfaz grafica
+	 * @param segundo_nodo segundo_nodo el segundo nodo que se selecciona e la interfaz grafica
+	 */
 	public void suma(Nodo primer_nodo, Nodo segundo_nodo) {
+		Nodo nodo_padre = null;
 		if ((primer_nodo.getValor() == segundo_nodo.getValor())&&(padreHijo(primer_nodo,segundo_nodo))) {
-			segundo_nodo.setValor(segundo_nodo.getValor()*2);
-			eliminar(raiz,primer_nodo);
+			nodo_padre = identificarPadre(primer_nodo,segundo_nodo);
+			if (nodo_padre == primer_nodo) {
+				primer_nodo.setValor(primer_nodo.getValor()*2);
+				eliminar(raiz,segundo_nodo);
+			}else if (nodo_padre == segundo_nodo) {
+				segundo_nodo.setValor(segundo_nodo.getValor()*2);
+				eliminar(raiz,primer_nodo);
+			}else if (nodo_padre == primer_nodo.getPadre()) {
+				segundo_nodo.setValor(segundo_nodo.getValor()*2);
+				eliminar(raiz,primer_nodo);
+			}
 		}
 	}
 }
--- ProyectoProgram2/src/proyecto/Nodo.java
+++ ProyectoProgram2/src/proyecto/Nodo.java
@@ -8,51 +8,91 @@ public class Nodo {
 	private int llave;
 	private int valor;
 	
+	/**
+	 * constructor vacio de la clase nodo
+	 */
 	public Nodo() {}
 	
+	/**
+	 * constructor de la clase nodo que usa un valor entero, al campo valor se le coloca 2 ya que es el numero base de las estructuras "burbujas"
+	 * @param indice indice del nodo
+	 */
 	public Nodo(int indice) {
 		this.llave = indice;
 		this.derecha = null;
 		this.izquierda = null;
-		this.valor = 0;
+		this.valor = 2;
 	}
-
+	
+	/**
+	 * get de la clase nodo para el padre
+	 * @return retorna el campo padre
+	 */
 	public Nodo getPadre() {
 		return padre;
 	}
-
+	
+	/**
+	 * set de la clase nodo para el campo 
+	 * @param padre es el nodo padre de este nodo
+	 */
 	public void setPadre(Nodo padre) {
 		this.padre = padre;
 	}
-
+	
+	/**
+	 * get del nodo hijo derecho de un nodo
+	 * @return devuelve el nodo hijo derecho del nodo
+	 */
 	public Nodo getDerecha() {
 		return derecha;
 	}
-
+	/**
+	 * set para colocar el nodo hijo derecha de un nodo
+	 * @param derecha nodo que sera el hijo derecho
+	 */
 	public void setDerecha(Nodo derecha) {
 		this.derecha = derecha;
 	}
-
+	/**
+	 * get del nodo hijo izquierdo de un nodo
+	 * @return devuelve el nodo hijo izquierdo del nodo
+	 */
 	public Nodo getIzquierda() {
 		return izquierda;
 	}
-
+	/**
+	 * set para colocar el nodo hijo izquierdo de un nodo
+	 * @param izquierda nodo que sera el hijo izquierdo
+	 */
 	public void setIzquierda(Nodo izquierda) {
 		this.izquierda = izquierda;
 	}
-
+	/**
+	 * get para el campo llave 
+	 * @return devuelve la llave del nodo
+	 */
 	public int getLlave() {
 		return llave;
 	}
-
+	/**
+	 * set para el campo llave 
+	 * @param llave valor que tendra el campo llave 
+	 */
 	public void setLlave(int llave) {
 		this.llave = llave;
 	}
-
+	/**
+	 * get para el campo valor del nodo
+	 * @return devuelve el valor que almacena el nodo 
+	 */
 	public int getValor() {
 		return valor;
 	}
-
+	/**
+	 * set para el campo valor
+	 * @param valor se usa para darle la cantidad que va a tener el nodo
+	 */
 	public void setValor(int valor) {
 		this.valor = valor;
 	}
